fce10eb090379249a83389522cbb0eab344be52b
Sabieh Ahmed <sabieh.ahmed@gmail.com>
2025-05-23 22:41:37 +0200

    Fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,55 @@
-import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import type {Metadata} from "next";
+import {GeistSans} from "geist/font/sans";
+import {GeistMono} from "geist/font/mono";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
+import {ThemeProvider} from "next-themes";
 import Footer from "@/components/footer";
 import Nav from "@/components/nav";
-import { config } from "@/lib/config"; // Import config
+import {config} from "@/lib/config";
 import ogImage from "./opengraph-image.png";
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: config.metadata.title,
-  description: config.metadata.description,
-  metadataBase: new URL(`https://www.sabiehahmed.com`),
-  openGraph: {
-    images: [
-      {
-        url: ogImage.src,
-        width: ogImage.width,
-        height: ogImage.height, 
-      },
-    ],
-  },
+    title: config.metadata.title,
+    description: config.metadata.description,
+    metadataBase: new URL(`https://www.sabiehahmed.com`),
+    openGraph: {
+        images: [
+            {
+                url: ogImage.src,
+                width: ogImage.width,
+                height: ogImage.height,
+            },
+        ],
+    },
 };
 
 export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                             children,
+                                         }: {
+    children: React.ReactNode;
 }) {
 
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <GoogleTagManager gtmId="G-SKRM16PJPS"/>
-      <body
-        className={`${GeistSans.variable} ${GeistMono.variable} relative font-sans bg-whiteout selection:text-white  selection:bg-pink-400 dark:bg-zinc-900 text-blackout dark:text-zinc-100`}
-      >
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SKRM16PJPS"/>
+        <Script id="gtag-init" strategy="afterInteractive">
+            {`window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SKRM16PJPS')`};
+        </Script>
+        <body
+            className={`${GeistSans.variable} ${GeistMono.variable} relative font-sans bg-whiteout selection:text-white  selection:bg-pink-400 dark:bg-zinc-900 text-blackout dark:text-zinc-100`}
+        >
         <ThemeProvider attribute="class">
-          <Nav/>
-          {children}
-          <Footer />
+            <Nav/>
+            {children}
+            <Footer/>
         </ThemeProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
 
 export const revalidate = 300;
